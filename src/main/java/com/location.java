bcc10e1c4f38f99b76f496315101547233619af8
Samuditha Dilanajan Wijayasundara <91152926+SamudithaD97@users.noreply.github.com>
2022-04-25 23:14:04 +0530

    insert

--- src/main/java/com/location.java
+++ src/main/java/com/location.java
@@ -16,3 +16,33 @@ private Connection connect()
  {e.printStackTrace();}
  return con;
  }
+public String insertItem(String Code, String Name, String Date, String Time)
+ {
+ String output = "";
+ try
+ {
+ Connection con = connect();
+ if (con == null)
+ {return "Error while connecting to the database for inserting."; }
+ // create a prepared statement
+ String query = " insert into items(`locationID`,`locationCode`,`locationName`,`date`,`time`)"
+ + " values (?, ?, ?, ?, ?)";
+ PreparedStatement preparedStmt = con.prepareStatement(query);
+ // binding values
+ preparedStmt.setInt(1, 0);
+ preparedStmt.setString(2, Code);
+ preparedStmt.setString(3, Name);
+ preparedStmt.setString(4, Date);
+ preparedStmt.setString(5, Time);
+ // execute the statement
+ preparedStmt.execute();
+ con.close();
+ output = "Inserted successfully";
+ }
+ catch (Exception e)
+ {
+ output = "Error while inserting the item.";
+ System.err.println(e.getMessage());
+ }
+ return output;
+ }
