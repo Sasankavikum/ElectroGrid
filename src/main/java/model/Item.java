eca49b70a2abb5aaea562cb119c880d0ad7666f8
Dulitha Kaluarachchi <88882564+dulitha98@users.noreply.github.com>
2022-04-25 21:46:11 +0530

    Update Item.java

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -136,5 +136,25 @@ public String updateItem(String ID, String userNumber, String name, String addre
 	 {
 	 String output = "";
 	 try
-	 
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for deleting."; }
+	 // create a prepared statement
+	 String query = "delete from user where userID=?";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, Integer.parseInt(userID));
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Deleted successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while deleting the item.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
 	}
